ec80774a4df06ce08272791b46769adc95e7fd30
Alexander Sierra <alexandercsierra@gmail.com>
2020-01-21 12:45:52 -0500

    Updates

---
+++ precache-manifest.aa40f83f984d6185c70f6be6e26ceebb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe212472b2e23224f580135101bcd256",
+    "url": "/React-Sorting-Hat/index.html"
+  },
+  {
+    "revision": "011f9516da15711c6a71",
+    "url": "/React-Sorting-Hat/static/css/main.b1360536.chunk.css"
+  },
+  {
+    "revision": "3772fc5bb91c58476f96",
+    "url": "/React-Sorting-Hat/static/js/2.6b55f581.chunk.js"
+  },
+  {
+    "revision": "011f9516da15711c6a71",
+    "url": "/React-Sorting-Hat/static/js/main.9b7fef9b.chunk.js"
+  },
+  {
+    "revision": "9866d44e97a368409119",
+    "url": "/React-Sorting-Hat/static/js/runtime-main.b47cc032.js"
+  },
+  {
+    "revision": "978cbf7389c6a0ebaf0e5c324cebc45b",
+    "url": "/React-Sorting-Hat/static/media/croppedbackground.978cbf73.jpg"
+  },
+  {
+    "revision": "949bcc9d07186366acede8d82db5522f",
+    "url": "/React-Sorting-Hat/static/media/gryff.949bcc9d.png"
+  },
+  {
+    "revision": "ced68abb1cec83f071e769252977f888",
+    "url": "/React-Sorting-Hat/static/media/harryp__-webfont.ced68abb.woff2"
+  },
+  {
+    "revision": "dde87578f403e97e2866f44cf5f22f92",
+    "url": "/React-Sorting-Hat/static/media/harryp__-webfont.dde87578.woff"
+  },
+  {
+    "revision": "093955fba76bfaf08ba037ded8772e61",
+    "url": "/React-Sorting-Hat/static/media/huff.093955fb.png"
+  },
+  {
+    "revision": "3bc430892b05aa1fab162aa6db1c120b",
+    "url": "/React-Sorting-Hat/static/media/raven.3bc43089.png"
+  },
+  {
+    "revision": "db11dc319270a76dd8ec59c7cb0ba84b",
+    "url": "/React-Sorting-Hat/static/media/slyth.db11dc31.png"
+  },
+  {
+    "revision": "9d9af0f0517dfd2b3d92b1977e0b1f8a",
+    "url": "/React-Sorting-Hat/static/media/sortinghat.9d9af0f0.jpg"
+  }
+]);
